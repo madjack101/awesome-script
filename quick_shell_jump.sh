b04c3dcd9f87f84fe0820d36edcc4c23f21399d8
madjack101 <madjack101>
2019-08-26 16:19:13 +0800

    add quick_navigator_shell, mark and jump

---
+++ quick_shell_jump.sh
@@ -0,0 +1,32 @@
+
+export MARKPATH=$HOME/.marks
+function jump {
+    cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
+}
+function mark {
+    mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
+}
+function unmark {
+    rm -i "$MARKPATH/$1"
+}
+function marks {
+    ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+}
+
+
+#function _completemarks {
+#  reply=($(ls $MARKPATH))
+#}
+#
+#compctl -K _completemarks jump
+#compctl -K _completemarks unmark
+#or the following to your .bashrc (thanks to microcolonel):
+
+_completemarks() {
+  local curw=${COMP_WORDS[COMP_CWORD]}
+  local wordlist=$(find $MARKPATH -type l -printf "%f\n")
+  COMPREPLY=($(compgen -W '${wordlist[@]}' -- "$curw"))
+  return 0
+}
+
+complete -F _completemarks jump unmark
